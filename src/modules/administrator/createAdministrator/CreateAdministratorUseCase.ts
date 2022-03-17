5ee8a5a321c1a3492acc4d88e8ad9b80e796abaf
Felipe Sarmento <felipeleaosarmento@gmail.com>
2022-03-17 12:22:42 -0300

    feat: password is now encrypted during creation

--- src/modules/administrator/createAdministrator/CreateAdministratorUseCase.ts
+++ src/modules/administrator/createAdministrator/CreateAdministratorUseCase.ts
@@ -1,4 +1,5 @@
 import { prisma } from "../../../database/prismaClient";
+import { hash } from "bcrypt";
 
 
 interface ICreateAdministrator {
@@ -11,12 +12,28 @@ interface ICreateAdministrator {
 
 export class CreateAdministratorUseCase {
   async execute({ name, username, email, password } : ICreateAdministrator) {
+    const adminExist = await prisma.administrator.findFirst({
+      where: {
+        username: {
+          mode: "insensitive"
+        }
+      }
+    })
+
+    if (adminExist) {
+      throw new Error("Administrator already exists!")
+    }
+
+    const hashNumber = parseInt(process.env.HASH_NUMBER as string);
+
+    const hashPassword = await hash(password, hashNumber)
+    
     const adm = await prisma.administrator.create({
       data: {
         name,
         username,
         email, 
-        password
+        password: hashPassword
       }
     })
     return adm;
